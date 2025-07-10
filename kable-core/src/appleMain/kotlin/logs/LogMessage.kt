1959329aa318ab75d0b336473da7fd8b25541e58
hamidmalek <hmdmlkv@gmail.com>
2025-07-10 14:36:13 +0200

    infer type before logging them

--- kable-core/src/appleMain/kotlin/logs/LogMessage.kt
+++ kable-core/src/appleMain/kotlin/logs/LogMessage.kt
@@ -3,6 +3,7 @@ package com.juul.kable.logs
 import com.juul.kable.logs.Logging.DataProcessor.Operation
 import com.juul.kable.toByteArray
 import com.juul.kable.toUuid
+import platform.CoreBluetooth.CBAttribute
 import platform.CoreBluetooth.CBCharacteristic
 import platform.CoreBluetooth.CBDescriptor
 import platform.CoreBluetooth.CBService
@@ -19,21 +20,30 @@ internal fun LogMessage.detail(error: NSError?) {
     if (error != null) detail("error", error.toString())
 }
 
-internal fun LogMessage.detail(service: CBService) {
+private fun LogMessage.detail(service: CBService) {
     detail("service", service.UUID.UUIDString)
 }
 
-internal fun LogMessage.detail(characteristic: CBCharacteristic) {
+private fun LogMessage.detail(characteristic: CBCharacteristic) {
     detail(
         characteristic.service!!.UUID.toUuid(),
         characteristic.UUID.toUuid(),
     )
 }
 
-internal fun LogMessage.detail(descriptor: CBDescriptor) {
+private fun LogMessage.detail(descriptor: CBDescriptor) {
     detail(
         descriptor.characteristic!!.service!!.UUID.toUuid(),
         descriptor.characteristic!!.UUID.toUuid(),
         descriptor.UUID.toUuid(),
     )
+}
+
+internal fun LogMessage.detail(attribute: CBAttribute) {
+    when (attribute) {
+        is CBService -> detail(attribute)
+        is CBCharacteristic -> detail(attribute)
+        is CBDescriptor -> detail(attribute)
+        else -> detail("unknown", attribute.UUID.toUuid())
+    }
 }
